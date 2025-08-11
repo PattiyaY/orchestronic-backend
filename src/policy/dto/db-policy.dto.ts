0af174cf67cacfa98a6306c45da1253f6566ebcf
PattiyaY <pattiya.2547@icloud.com>
2025-08-11 14:29:39 +0700

    refactor: update DB Policy Dto

--- src/policy/dto/db-policy.dto.ts
+++ src/policy/dto/db-policy.dto.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class DBPolicyDto {
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({
+    example: 50,
+    description: 'The maximum storage (in GB) allocated for the database',
+  })
   maxStorage: number;
 }
