13fa8318016bb880c904a614aa22e8936f6ed00f
Jan Poonthong <janpoonthong628@gmail.com>
2025-09-18 02:51:24 +0700

    fix: Update key name in write_to_db function to use project_name for PEM file path

--- dags/AWS_provide_vm.py
+++ dags/AWS_provide_vm.py
@@ -410,7 +410,7 @@ def write_to_db(terraform_dir, configInfo):
     )
     vmInstances = configInfo["vm_instances"]
     for i in range(len(vmInstances)):
-        pem_path = Path(terraform_dir) / f"{repoName}_{i+1}.pem"
+        pem_path = Path(terraform_dir) / f"{configInfo['project_name']}_{i+1}.pem"
         if pem_path.exists():
             with open(pem_path, 'r') as f:
                 pem_content = f.read()
